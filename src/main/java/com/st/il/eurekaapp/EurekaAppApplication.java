367b3457de967230e29f9d6803a410f5d6c1f5b1
Al-amine AHMED MOUSS <Al-amine AHMED MOUSS@HP-AAM>
2019-04-24 02:52:20 +0100

    changed all package names part2

--- src/main/java/com/st/il/eurekaapp/EurekaAppApplication.java
+++ src/main/java/com/st/il/eurekaapp/EurekaAppApplication.java
@@ -1,4 +1,4 @@
-package com.st.lmsEureka;
+package com.st.il.eurekaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class LmsEurekaApplication {
+public class EurekaAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LmsEurekaApplication.class, args);
+		SpringApplication.run(EurekaAppApplication.class, args);
 	}
 }
